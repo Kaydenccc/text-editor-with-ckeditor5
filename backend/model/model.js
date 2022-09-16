df0fb61f30a6a46c68bbc0ea00363bd210965d9c
Kaydenccc <ffadilah682@gmail.com>
2022-09-16 12:47:21 +0800

    memperbaiki bug pada text editor

--- backend/model/model.js
+++ backend/model/model.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 //membuat Schema
-const Schema = new mongoose.Schema({
-  blog: {
-    type: String,
-    required: true,
+const Schema = new mongoose.Schema(
+  {
+    blog: {
+      type: String,
+      required: true,
+    },
+    thumnail: String,
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Blog', Schema);
--- backend/router/router.js
+++ backend/router/router.js
@@ -4,27 +4,36 @@ const Model = require('../model/model');
 const path = require('path');
 
 // post untuk menghandle upload file image
+var filePath = '';
 router.post('/post', (req, res) => {
   console.log('BODY ::', req.body);
-  if (!req.body) return res.status(402).json({ msg: 'Bad Request' });
-  console.log(req.files.upload);
-  // get upload file
-  let file = req.files.upload;
-
-  // filter file image
-  if (path.extname(file.originalFilename).toLocaleLowerCase() === '.png' || '.jpg' || '.jpeg') {
-    res.status(201).json({ uploaded: true, url: `http://localhost:4000/${file.path}` });
+  if (req.files) {
+    try {
+      if (!req.body) return res.status(402).json({ msg: 'Bad Request' });
+      console.log(req.files.upload);
+      // get upload file
+      const file = req.files.upload;
+      // get the file path for store into DB
+      filePath = file.path;
+      // filter file image
+      if (path.extname(file.originalFilename).toLocaleLowerCase() === '.png' || '.jpg' || '.jpeg') {
+        res.status(201).json({ uploaded: true, url: `http://localhost:4000/${file.path}` });
+      }
+    } catch (err) {
+      return res.status(err).json({ msg: err.message });
+    }
+  } else {
+    // post untuk menghendel content pada inputan user
+    console.log('debug:', req.body);
+    console.log('[Path]:', filePath);
+    if (!req.body) return res.status(402).json({ msg: 'Bad Request' });
+    Model.create({ blog: req.body.blog, thumnail: filePath }, (err, data) => {
+      if (err) return res.status(400).json({ msg: 'Bad Request' });
+      res.status(201).json({ msg: 'OK', data });
+    });
+    filePath = '';
   }
-});
-
-// post untuk menghendel content pada inputan user
-router.post('/content', (req, res) => {
-  console.log('debug:', req.body);
-  if (!req.body) return res.status(402).json({ msg: 'Bad Request' });
-  Model.create({ blog: req.body.blog }, (err, data) => {
-    if (err) return res.status(400).json({ msg: 'Bad Request' });
-    res.status(201).json({ msg: 'OK', data });
-  });
+  console.log('POST filePath:', filePath);
 });
 
 // get data from db to client
@@ -44,4 +53,16 @@ router.get('/get/content/:id', (req, res) => {
   });
 });
 
+// update data
+router.put('/put/content/:id', (req, res) => {
+  console.log('update filePath:', filePath);
+  console.log('Update id:', req.params); ///------ data yang diupdate------
+  Model.findOne({ _id: req.params.id }, (err, data) => {
+    Model.findByIdAndUpdate({ _id: data._id }, { blog: req.body.blog, thumnail: filePath || data.thumnail }, (err, data) => {
+      if (err) return res.status(400).json({ msg: 'Not Found Data' });
+      res.status(200).json({ msg: 'UPDATE OK', data });
+      filePath = '';
+    });
+  });
+});
 module.exports = router;
